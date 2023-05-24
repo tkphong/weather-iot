da362133d1a905eccc502865b013a9f59a85c8a9
Tran Phong <fong1668vn@gmail.com>
2023-05-24 17:32:26 +0700

    add file config

--- src/config.h
+++ src/config.h
@@ -4,9 +4,8 @@
 #include "Adafruit_MQTT.h"
 #include "Adafruit_MQTT_Client.h"
 #include <Wifi.h>
-
-
-
+#include <esp_sleep.h>
+#include <PubSubClient.h>
 // WiFi parameters
 #define WLAN_SSID       "A1"
 #define WLAN_PASS       "88889999"
@@ -14,7 +13,7 @@
 #define AIO_SERVER      "io.adafruit.com"
 #define AIO_SERVERPORT  1883
 #define AIO_USERNAME    "fong1668vn"
-#define AIO_KEY         "aio_zzcI95MOqmscjWcueRzccjnMYpox" 
+#define AIO_KEY         "aio_eRsC597mEzUiQK2CpC6yektTtBMp" 
 WiFiClient client;
 
 #define DHTPIN 4
@@ -33,8 +32,17 @@ const int lightPin = 34;
 const int soundPin = 35;
 const int gasPin = 25;
 
+
 /*Config Adafruit*/
-Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
-Adafruit_MQTT_Publish Temperature1 = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/weather-temp");
-Adafruit_MQTT_Publish Humidity1 = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/weather-humi");
-Adafruit_MQTT_Publish Sound1 = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/weather-wind");
+// Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
+// Adafruit_MQTT_Publish Temperature1 = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/data-sensor.weather-temperature");
+// Adafruit_MQTT_Publish Humidity1 = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME "/feeds/data-sensor.weather-humidity");
+
+/*Config Mqtt Broker*/
+#define MQTT_SERVER "broker.emqx.io"
+#define MQTT_PORT 1883
+#define MQTT_USERNAME "weather"
+#define MQTT_PASSWORD "123456Pp"
+#define MQTT_TOPIC_TEMPERATURE "/topic/temperature"
+#define MQTT_TOPIC_HUMIDITY "/topic/humidity"
+PubSubClient mqtt(client);
